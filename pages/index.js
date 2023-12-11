7f04795ed6daa545f9f0b4f09752571ca46d55e4
Horace <49237174+MHieu128@users.noreply.github.com>
2023-12-11 13:36:15 +0700

    init project

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <div>Next JS &amp; WordPress course.</div>;
+import client from "client";
+import { gql } from "@apollo/client";
+
+export default function Home(props) {
+  console.log(props);
+  let image = props.blocks[0].attributes.url;
+  console.log(image);
+  return <div>Next JS &amp; WordPress.</div>;
+}
+
+export const getStaticProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query NewQuery {
+        nodeByUri(uri: "/") {
+          ... on Page {
+            id
+            blocks(postTemplate: false)
+          }
+        }
+      }
+      `,
+  });
+  return {
+    props: {
+      blocks: data.nodeByUri.blocks
+    }
+  }
 }
